724f85224ea0c55984609a58ebb21dc0bf9619e8
Yubin Lee <147678045+ubinyong@users.noreply.github.com>
2025-09-05 12:48:02 +0900

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,12 @@
 const themeMap = {
   'default-light': {
-    'background-color': '#8cded4',
+    'background-color': '#faf9f6',
     'text-color': '#1a1a1a',
     'highlight-color': '#8cded4'
   },
   'default-dark': {
     'background-color': '#1a1a1a',
-    'text-color': '#8cded4',
+    'text-color': '#faf9f6',
     'highlight-color': '#8cded4'
   }
 }
