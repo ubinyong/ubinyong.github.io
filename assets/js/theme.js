29d3b0762d2d7c6d6263e2de0710b1f9aa894550
Yubin Lee <147678045+ubinyong@users.noreply.github.com>
2025-09-05 12:42:39 +0900

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,32 +1,12 @@
 const themeMap = {
   'default-light': {
-    'background-color': 'white',
-    'text-color': '#222',
-    'highlight-color': '#eee'
+    'background-color': '#8cded4',
+    'text-color': '#1a1a1a',
+    'highlight-color': '#8cded4'
   },
   'default-dark': {
-    'background-color': '#222',
-    'text-color': 'white',
-    'highlight-color': '#2e2e2e'
-  },
-  'solarized-light': {
-    'background-color': '#fdf6e3',
-    'text-color': '#657b83',
-    'highlight-color': '#eee8d5'
-  },
-  'solarized-dark': {
-    'background-color': '#002b36',
-    'text-color': '#839496',
-    'highlight-color': '#073642'
-  },
-  'sepia-light': {
-    'background-color': '#f4ecd8',
-    'text-color': '#5b4636',
-    'highlight-color': '#e4d5b7'
-  },
-  'sepia-dark': {
-    'background-color': '#3e2c1c',
-    'text-color': '#d8c3a5',
-    'highlight-color': '#4b382a'
+    'background-color': '#1a1a1a',
+    'text-color': '#8cded4',
+    'highlight-color': '#8cded4'
   }
 }
